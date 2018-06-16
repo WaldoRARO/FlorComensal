44a219ec29c6a20b6bcbba06c7471d34d820ee9a
hernja <hernja666@gmail.com>
2018-06-16 00:00:17 -0500

    update

--- reservacion.php
+++ reservacion.php
@@ -45,48 +45,47 @@
                   <form>
                             <div class="form-row">
                                 <div class="form-group col-md-6">
-                                  <label for="inputEmail4">Email</label>
-                                  <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+                                  <label for="inputEmail4">Nombre</label>
+                                  <input type="text" class="form-control" id="inputEmail4" placeholder="Nombre">
                                 </div>
                                 <div class="form-group col-md-6">
-                                  <label for="inputPassword4">Password</label>
-                                  <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+                                  <label for="inputPassword4">Correo</label>
+                                  <input type="email" class="form-control" id="inputPassword4" placeholder="Correo">
                                 </div>
-                            </div>
-                            <div class="form-group">
-                                  <label for="inputAddress">Address</label>
-                                  <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-                            </div>
-                            <div class="form-group">
-                                <label for="inputAddress2">Address 2</label>
-                                <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
                             </div>
                             <div class="form-row">
-                                <div class="form-group col-md-6">
-                                  <label for="inputCity">City</label>
-                                  <input type="text" class="form-control" id="inputCity">
+                                <div class="form-group col-md-3">
+                                  <label for="inputEmail4">Telefono</label>
+                                  <input type="text" class="form-control" id="inputEmail4" placeholder="Telefono">
                                 </div>
-                                <div class="form-group col-md-4">
-                                  <label for="inputState">State</label>
+                                <div class="form-group col-md-3">
+                                  <label for="inputPassword4">Numero de Personas</label>
+                                  <input type="text" class="form-control" id="inputPassword4" placeholder="Correo">
+                                </div>
+                                <div class="form-group col-md-3">
+                                  <label for="inputPassword4">Tipo de Evento</label>
                                   <select id="inputState" class="form-control">
-                                    <option selected>Choose...</option>
-                                    <option>...</option>
+                                    <option selected></option>
+                                    <option>Casual</option>
+                                    <option>Negocios</option>
+                                    <option>Aniversario</option>
+                                    <option>Compromiso</option>
                                   </select>
                                 </div>
-                                <div class="form-group col-md-2">
-                                  <label for="inputZip">Zip</label>
-                                  <input type="text" class="form-control" id="inputZip">
+                            </div>
+                            <div class="form-row">
+                                <div class="form-group col-md-3">
+                                      <label for="inputPassword4">Fecha</label>
+                                      <br>
+                                    <form action="" method="">
+                                      <input type="date" >
+                                    </form>
+                                    <br>
+                                    <br>
+                                      <button type="submit" class="btn btn-primary">Enviar</button>
                                 </div>
                             </div>
-                            <div class="form-group">
-                                <div class="form-check">
-                                  <input class="form-check-input" type="checkbox" id="gridCheck">
-                                  <label class="form-check-label" for="gridCheck">
-                                    Check me out
-                                  </label>
-                                </div>
                             </div>
-                            <button type="submit" class="btn btn-primary">Sign in</button>
                   </form>
               </div>
           </div>
